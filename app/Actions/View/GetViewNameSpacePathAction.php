bee16ee56e184e640bf774ced9b55005e32ad699
marco76tv <marco.sottana@gmail.com>
2025-05-13 08:44:31 +0200

    .

--- app/Actions/View/GetViewNameSpacePathAction.php
+++ app/Actions/View/GetViewNameSpacePathAction.php
@@ -23,7 +23,7 @@ class GetViewNameSpacePathAction
         if (null !== $module_name && '' !== $module_name) {
             $module_path = Module::getModulePath($module_name);
             /** @var non-falsy-string $namespace_path */
-            $namespace_path = $module_path.'Resources/views';
+            $namespace_path = $module_path.'resources/views';
         } else {
             /** @var non-falsy-string $namespace_path */
             $namespace_path = resource_path('views');
