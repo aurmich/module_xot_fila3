4902a6671dc6e17503cc58cf10fc5e923cbbab7b
Malebestia <malebestia@yahoo.it>
2025-05-20 10:22:00 +0200

    .

--- lang/it/set_default_tenant_for_urls.php
+++ lang/it/set_default_tenant_for_urls.php
@@ -1,40 +1,49 @@
 <?php
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 return array (
   'actions' => 
   array (
     'authenticate' => 
     array (
+=======
+declare(strict_types=1);
+return array(
+  'actions' =>
+  array(
+    'authenticate' =>
+    array(
+>>>>>>> 1691e12 (.)
       'label' => 'authenticate',
     ),
-    'login' => 
-    array (
+    'login' =>
+    array(
       'label' => 'login',
     ),
-    'request' => 
-    array (
+    'request' =>
+    array(
       'label' => 'request',
     ),
   ),
-  'fields' => 
-  array (
-    'email' => 
-    array (
+  'fields' =>
+  array(
+    'email' =>
+    array(
       'label' => 'email',
       'description' => 'email',
       'helper_text' => 'email',
       'placeholder' => 'email',
     ),
-    'password' => 
-    array (
+    'password' =>
+    array(
       'label' => 'password',
       'description' => 'password',
       'helper_text' => 'password',
       'placeholder' => 'password',
     ),
-    'remember' => 
-    array (
+    'remember' =>
+    array(
       'label' => 'remember',
       'description' => 'remember',
       'helper_text' => 'remember',
