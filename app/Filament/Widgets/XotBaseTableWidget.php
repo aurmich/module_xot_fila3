a01f25351cfaf1dd2c86a08426353f549e86c293
marco76tv <marco.sottana@gmail.com>
2025-08-19 15:26:57 +0200

    .

--- app/Filament/Widgets/XotBaseTableWidget.php
+++ app/Filament/Widgets/XotBaseTableWidget.php
@@ -2,18 +2,27 @@
 
 namespace Modules\Xot\Filament\Widgets;
 <<<<<<< HEAD
+<<<<<<< HEAD
 use Filament\Tables\Table;
 use Modules\Xot\Filament\Traits\TransTrait;
 use Modules\Xot\Filament\Traits\HasXotTable;
 =======
 use Modules\Xot\Filament\Traits\TransTrait;
 >>>>>>> 995f7cae (.)
+=======
+use Filament\Tables\Table;
+use Modules\Xot\Filament\Traits\TransTrait;
+use Modules\Xot\Filament\Traits\HasXotTable;
+>>>>>>> 75fe9751 (.)
 use Filament\Widgets\TableWidget as FilamentTableWidget;
 
 abstract class XotBaseTableWidget extends FilamentTableWidget
 {
     use TransTrait;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 75fe9751 (.)
     use HasXotTable;
 
     public function table(Table $table): Table
@@ -21,6 +30,9 @@ abstract class XotBaseTableWidget extends FilamentTableWidget
         return parent::table($table)
         ->query($this->getTableQuery());
     }
+<<<<<<< HEAD
 =======
 >>>>>>> 995f7cae (.)
+=======
+>>>>>>> 75fe9751 (.)
 }
